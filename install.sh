68098d0affeebef62b666cd07be0ca462e83ea1b
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-07-23 11:53:13 +0530

    Update and rename add-docker-repo to install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,14 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the system
 sudo apt-get update
+
+# Download Docker
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+
+# Install
+sudo apt-get install ./docker-desktop-*.deb -y
+
 
